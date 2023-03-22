db4395673d110d6096dc9f6835d30202717802ab
Martindev3 <martinedgardoquiroga@gmail.com>
2023-03-22 16:12:06 -0300

    Agregando el Proyecto

---
+++ clima/src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+      <div className='Loading'>
+        <div className="spinner">
+           <div className="dot1"></div>
+           <div className="dot2"></div>
+           </div>
+     
+
+    </div>
+  )
+}
+
+export default Loading
